a997277d4ee4846e3d01f5d6f487cc4039f29f3b
Aaron Sachs <aaronm.sachs@gmail.com>
2017-07-11 13:29:32 -0500

    Added door monitor

--- ruby/door_status.rb
+++ ruby/door_status.rb
@@ -3,12 +3,17 @@ require 'rpi_gpio'
 RPi::GPIO.set_numbering :bcm
 
 PIN_NUM = 5
-RPi::GPIO.setup PIN_NUM, :as => :input
 
+#RPi::GPIO.setup PIN_NUM, :as => :input
+RPi::GPIO.setup PIN_NUM, :as => :input, :pull => :up	
 
-door_status = RPi::GPIO.setup PIN_NUM, :as => :input, :pull => :up
-
-
-while door_status == true do
-	print("Kegerator door is Open! Close it!")
+while true do
+	if RPi::GPIO.high? PIN_NUM
+	#RPi::GPIO.setup PIN_NUM, :as => :input, :pull => :up	
+		puts("Kegerator door is open! Close it!")
+		sleep 2.0
+	else
+		puts("Kegerator door is closed.")
+	sleep 2.0
+	end
 end
