3e0afc9e4d9e8befacad9b469292253952e17e31
Aaron Sachs <aaronm.sachs@gmail.com>
2017-07-12 12:58:50 -0500

    Updates

--- ruby/door_status2.rb
+++
@@ -1,8 +0,0 @@
-require 'pi_piper'
-include PiPiper
-
-watch :pin => 5 do
-		puts "Pin changed from closed to open"
-end
-
-PiPiper.wait
--- ruby/get_temp.rb
+++ ruby/get_temp.rb
@@ -1,3 +1,40 @@
 require 'os'
 require 'rpi_gpio'
 
+system("modprobe w1-gpio")
+system("modprobe w1-therm")
+
+#basedir = "/sys/bus/w1/devices/"
+#device_folder = Dir.glob(basedir + "28*")[0]
+#device_file = device_folder + "/w1_slave"
+
+def read_temp_raw()
+  basedir = "/sys/bus/w1/devices/" 
+  device_folder = Dir.glob(basedir + "28*")[0]
+  device_file = device_folder + "/w1_slave"
+  f = open(device_file, 'r')
+  lines = f.read()
+  f.close()
+  #return lines
+end
+
+def read_temp()
+  lines = read_temp_raw()
+  while lines[0].strip()[-3:] != "YES" do
+	sleep 0.2
+	lines = read_temp_raw
+  end
+  equals_pos = lines[1].find("t=")
+  if equals_pos != -1 do
+	temp_string = lines[1][equals_pos+2:]
+	temp_c = temp_string.to_f / 1000.0
+	temp_f = temp_c * 9.0 / 5.0 + 32.0
+	return temp_f
+  end
+end
+
+while true do
+  p("Current Temp: " + read_temp().to_s + " Fahrenheit")
+  sleep 1.0
+end
+
