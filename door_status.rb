7b172c30eb6c85c2d9486fcd2e82c3cece71a666
Aaron Sachs <aaronm.sachs@gmail.com>
2017-07-18 09:51:10 -0500

    Fixing hard tabs

--- door_status.rb
+++ door_status.rb
@@ -6,11 +6,11 @@ PIN_NUM = 5
 
 RPi::GPIO.setup PIN_NUM, :as => :input, :pull => :up	
 while true do
-	if RPi::GPIO.high? PIN_NUM
-	  puts("Kegerator door is open! Close it!")
-	  sleep 2.0
-	else
-	  puts("Kegerator door is closed.")
-	  sleep 2.0
-	end
+  if RPi::GPIO.high? PIN_NUM
+    puts("Kegerator door is open! Close it!")
+    sleep 2.0
+  else
+    puts("Kegerator door is closed.")
+    sleep 2.0
+  end
 end
--- get_temp.rb
+++ get_temp.rb
@@ -18,15 +18,15 @@ end
 def read_temp()
   lines = read_temp_raw()
   while lines[0][-4..-2] != "YES" do
-	sleep 0.2
-	lines = read_temp_raw
+    sleep 0.2
+    lines = read_temp_raw
   end
   equals_pos = lines[1].index("t=")
   if equals_pos != -1 then
-		  temp_string = lines[1][equals_pos+2..-1].chomp
-	temp_c = temp_string.to_f / 1000.0
-	temp_f = temp_c * 9.0 / 5.0 + 32.0
-	return temp_f
+    temp_string = lines[1][equals_pos+2..-1].chomp
+    temp_c = temp_string.to_f / 1000.0
+    temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return temp_f
   end
 end
 
